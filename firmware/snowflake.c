16d6351ae15570cfdf4b0aa6cf2feb56ed38e32b
David Turner <cillian64@googlemail.com>
2016-12-17 20:20:11 +0000

    Even lower power mode using standby

--- firmware/snowflake.c
+++ firmware/snowflake.c
@@ -5,6 +5,8 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/pwr.h>
+#include <libopencm3/stm32/iwdg.h>
 
 #define LED1 GPIO0
 #define LED2 GPIO1
@@ -38,6 +40,12 @@ static const uint32_t led_ports[] = {
 #define TURNON(led) gpio_set(led_ports[led], led_pins[led])
 #define TURNOFF(led) gpio_clear(led_ports[led], led_pins[led])
 
+//#define RTC_BKP0R MMIO32(RTC_BASE + 0x50)
+#define RTC_BKP0R MMIO32(0x40002860)
+
+#define SCB_SCR MMIO32(SCB_BASE + 0x10)
+#define SCB_SCR_SLEEPDEEP (1<<2)
+
 static void gpio_setup(void)
 {
     int led;
@@ -65,26 +73,16 @@ static void shortwait(void)
     // About 0.25ms
     timer_set_period(TIM14, 1);
     timer_enable_counter(TIM14);
+    SCB_SCR &= ~SCB_SCR_SLEEPDEEP;
     __asm__("wfi");
     timer_disable_counter(TIM14);
 }
 
-static void longwait(void)
+static void standby(void)
 {
-    // About 100ms
-    timer_set_period(TIM14, 400u);
-    timer_enable_counter(TIM14);
+    SCB_SCR |= SCB_SCR_SLEEPDEEP;
+    pwr_set_standby_mode();
     __asm__("wfi");
-    timer_disable_counter(TIM14);
-}
-
-static void longlongwait(void)
-{
-    // About 1000ms
-    timer_set_period(TIM14, 4000u);
-    timer_enable_counter(TIM14);
-    __asm__("wfi");
-    timer_disable_counter(TIM14);
 }
 
 int main(void)
@@ -104,49 +102,67 @@ int main(void)
     timer_enable_update_event(TIM14);
     timer_disable_counter(TIM14);
 
-    while(true)
+    rcc_periph_clock_enable(RCC_PWR);
+    rcc_periph_clock_enable(RCC_RTC);
+    pwr_disable_backup_domain_write_protect();
+
+    iwdg_reset();
+
+    if(RTC_BKP0R > 7)
+        RTC_BKP0R = 0;
+
+    switch(RTC_BKP0R)
     {
-        TURNON(1);
-        shortwait();
-        TURNOFF(1);
-        longwait();
-
-        TURNON(4);
-        shortwait();
-        TURNOFF(4);
-        longwait();
-
-        TURNON(6);
-        shortwait();
-        TURNOFF(6);
-        longwait();
-
-        TURNON(2);
-        shortwait();
-        TURNOFF(2);
-        longwait();
-
-        TURNON(8);
-        shortwait();
-        TURNOFF(8);
-        longwait();
-
-        TURNON(5);
-        shortwait();
-        TURNOFF(5);
-        longwait();
-
-        TURNON(7);
-        shortwait();
-        TURNOFF(7);
-        longwait();
-
-        TURNON(3);
-        shortwait();
-        TURNOFF(3);
-        longwait();
-
-        longlongwait();
+        case 0:
+            TURNON(1);
+            shortwait();
+            TURNOFF(1);
+            break;
+        case 1:
+            TURNON(4);
+            shortwait();
+            TURNOFF(4);
+            break;
+        case 2:
+            TURNON(6);
+            shortwait();
+            TURNOFF(6);
+            break;
+        case 3:
+            TURNON(2);
+            shortwait();
+            TURNOFF(2);
+            break;
+        case 4:
+            TURNON(8);
+            shortwait();
+            TURNOFF(8);
+            break;
+        case 5:
+            TURNON(5);
+            shortwait();
+            TURNOFF(5);
+            break;
+        case 6:
+            TURNON(7);
+            shortwait();
+            TURNOFF(7);
+            break;
+        case 7:
+            TURNON(3);
+            shortwait();
+            TURNOFF(3);
+            break;
     }
+
+    RTC_BKP0R += 1;
+    if(RTC_BKP0R == 8)
+        RTC_BKP0R = 0;
+
+    iwdg_reset();
+    iwdg_set_period_ms(100);
+    iwdg_start();
+    standby();
+
     return 0;
 }
